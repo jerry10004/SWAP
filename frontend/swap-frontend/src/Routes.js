633b33e1210aa4609c05ee17fc20f46c1d0ffe21
leehyelim <qazxcv0691@naver.com>
2022-03-20 11:29:21 +0900

    feat[#24]: Program 레이아웃 생성

--- frontend/swap-frontend/src/Routes.js
+++ frontend/swap-frontend/src/Routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "assets/scss/theme.scss";
 
 import Main from "./pages/Main";
+import Program from "./pages/Program";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/program" element={<Program />} />
       </Routes>
     </BrowserRouter>
   );
